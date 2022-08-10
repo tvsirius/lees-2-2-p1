37e4759091d65cd41fb22cb4245d219f0d1cee26
Denis Savitsky <densav@gmail.com>
2022-08-10 20:58:51 +0300

    threading

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import threading
 from time import sleep
 
-def cook(name):
-    print(f'Подготовить блюдо {name}')
-    print(f'Поставить {name} на плиту')
-    sleep(20)
-    print(f'Блюдо {name} готово')
 
-cook('Лапша')
-cook('Курка')
-cook('Пельмени')
+x=[]
 
+
+def cook(name='блюдо', t=5):
+    print(f'Начали готовить {name}')
+    sleep(t)
+    print(f'{name} готово!')
+
+t1 = threading.Thread(target=cook, args=('Борщ',))
+t2 = threading.Thread(target=cook, args=('Пельмени',))
+t3 = threading.Thread(target=cook, args=('Плов',))
+
+#
+# cook("Борщ")
+# cook("Пельмени")
+# cook("Плов")
+
+t1.start()
+t2.start()
+t3.start()
